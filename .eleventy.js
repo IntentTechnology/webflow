8e83bdb10f0bbb4c49baa5824ae90f2e006840ba
intent <jesutomidaniel@gmail.com>
2023-12-21 00:23:51 +0100

    changed settings

--- .eleventy.js
+++ .eleventy.js
@@ -3,13 +3,11 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/admin");
 
   return {
-    passthroughFileCopy: true,
     markdownTemplateEngine: "njk",
     templateFormats: ["html", "njk", "md"],
     dir: {
       input: "src",
       output: "_site",
-      include: "includes",
     },
   };
 };
